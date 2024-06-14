74456f30cb597c69d09198a3e850f5c0f1eefed6
unknown <you@example.com>
2024-06-14 16:54:57 -0400

    actulizacion de search

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
+import { useEffect, useState } from 'react'
 import { Card } from './components/Card'
 import { DetailsPerfil } from './components/DetailsPerfil'
-import { Seach } from './components/Seach'
+import heroImagen from './assets/hero-image-github-profile.png'
+
 
 function App() {
+  const[data, setData] = useState(null)
+  const[user, setUser] = useState('')
 
+  const fetchUser = async() =>{
+    try {
+      const rs = await fetch(`https://api.github.com/users/${user}`)
+    const rsJson = await rs.json()
+    setData(rsJson)
+
+    } catch (error) {
+      console.log('Error fetching Data', error.message)
+      
+    }
+    
+  }
 
   return (
+
     <div className='min-h-screen bg-[#20293A]'>
-      <Seach />
-      <DetailsPerfil />
-      <Card/>
+      <div className='relative  '>
+        <figure>
+          <img src={heroImagen} alt="heroImagen" className='h-[40vh] w-full' />
+        </figure>
+        <div className='flex justify-center absolute top-[5%] w-full h-max'>
+
+          <button onClick={fetchUser} className='h-16 w-16 p-5 rounded-lg-md bg-[#20293a]'>
+            <svg className='bg-[#20293a]' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="11" cy="11" r="7" stroke="#97A3B6"  />
+              <path d="M20 20L17 17" stroke="#97A3B6" />
+            </svg>
+          </button>
+
+          
+          <input onChange={(e) => setUser(e.target.value)} type="text" className=' p-3 h-16 w-1/3  bg-[#20293a] rounded-r-lg outline-none text-white' placeholder='Username' />
+        </div>
+      </div>
+      <DetailsPerfil data={data}/>
+      <Card />
     </div>
+
+
+
   )
 }
 
--- src/components/DetailsPerfil.jsx
+++ src/components/DetailsPerfil.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import gitHub from '../assets/github.jpg'
 
-export const DetailsPerfil = () => {
+export const DetailsPerfil = ({data}) => {
 
 
     
   return (
 <>
-<div className='flex justify-evenly w-full h-40  text-[#CDD5E0] '>
+    {data &&(
+        <div className='flex justify-evenly w-full h-40  text-[#CDD5E0] '>
         <div className='w- flex flex-col'>
           <figure className='w-32 h-32 absolute top-[33%]'>
-            <img src={gitHub} alt="GitHub" className='rounded-lg border-solid border-[10px] border-[#20293A] ' />
+            <img src={data.avatar_url} alt="GitHub" className='rounded-lg border-solid border-[10px] border-[#20293A] ' />
           </figure>
           <h1 className='font-bold text-5xl  mt-20 mb-2'>GitHub</h1>
           <p className='text-xl'>How people build software</p>
@@ -18,19 +19,25 @@ export const DetailsPerfil = () => {
 
         <div className='mt-5 bg-[#111729] px-4 py-2 rounded-lg h-14 w-60 flez items-center text-2xl'>
           <span className='mr-10 font-normal text-[#4a5366] '>Followers</span>
-          <span className='ml-5'>2345</span>
+          <span className='ml-5'>{data.followers}</span>
         </div>
 
         <div className='mt-5 bg-[#111729] px-4 py-2 rounded-lg h-14 w-60 flez items-center text-2xl'>
           <span className='mr-10 font-normal text-[#4a5366] '>Following</span>
-          <span className='ml-5'>2345</span>
+          <span className='ml-5'>{data.following}</span>
         </div>
 
-        <div className='mt-5 bg-[#111729] px-4 py-2 rounded-lg h-14 w-[350px] flez items-center text-2xl'>
+        <div className='mt-5 bg-[#111729] px-4 py-2 rounded-lg h-14 max-w flez items-center text-2xl'>
           <span className='mr-5 font-normal text-[#4a5366] '>Location</span>
-          <span className='ml-5'>San Francisco, CA</span>
+          <span className='ml-5'>{data.location}</span>
         </div>
       </div>
+
+    )
+
+    }
+  
+
 </>
   )
 }
