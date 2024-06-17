0cdf87763ceefdb4c68baaede1f8141aec2c1287
unknown <you@example.com>
2024-06-17 12:58:27 -0400

    finalizcion del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,73 @@
-import { useEffect, useState } from 'react'
-import { Card } from './components/Card'
-import { DetailsPerfil } from './components/DetailsPerfil'
-import heroImagen from './assets/hero-image-github-profile.png'
-
+import React, { useState } from 'react';
+import { Card } from './components/Card';
+import { DetailsPerfil } from './components/DetailsPerfil';
+import heroImagen from './assets/hero-image-github-profile.png';
 
 function App() {
-  const[data, setData] = useState(null)
-  const[user, setUser] = useState('')
+  const [data, setData] = useState(null);
+  const [user, setUser] = useState('');
+  const [repos, setRepos] = useState([]);
 
-  const fetchUser = async() =>{
+  const fetchUser = async () => {
     try {
-      const rs = await fetch(`https://api.github.com/users/${user}`)
-    const rsJson = await rs.json()
-    setData(rsJson)
-
+      const rs = await fetch(`https://api.github.com/users/${user}`);
+      const rsJson = await rs.json();
+      setData(rsJson);
+      fetchRepos();
     } catch (error) {
-      console.log('Error fetching Data', error.message)
-      
+      console.log('Error fetching Data', error.message);
     }
-    
-  }
+  };
+
+  const fetchRepos = async () => {
+    try {
+      const rs = await fetch(`https://api.github.com/users/${user}/repos`);
+      if (rs.ok) {
+        const reposData = await rs.json();
+        setRepos(reposData);
+      } else {
+        console.error('Failed to fetch user repositories:', rs.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching user repositories:', error.message);
+    }
+  };
 
   return (
-
     <div className='min-h-screen bg-[#20293A]'>
-      <div className='relative  '>
+      <div className='relative'>
         <figure>
-          <img src={heroImagen} alt="heroImagen" className='h-[40vh] w-full' />
+          <img src={heroImagen} alt='heroImagen' className='h-[40vh] w-full object-cover' />
         </figure>
-        <div className='flex justify-center absolute top-[5%] w-full h-max'>
-
-          <button onClick={fetchUser} className='h-16 w-16 p-5 rounded-lg-md bg-[#20293a]'>
-            <svg className='bg-[#20293a]' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="11" cy="11" r="7" stroke="#97A3B6"  />
-              <path d="M20 20L17 17" stroke="#97A3B6" />
+        <div className='flex justify-center absolute top-[5%] w-full'>
+          <button onClick={fetchUser} className='h-16 w-16 p-5 rounded-l-lg bg-[#20293a]'>
+            <svg
+              className='bg-[#20293a]'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <circle cx='11' cy='11' r='7' stroke='#97A3B6' />
+              <path d='M20 20L17 17' stroke='#97A3B6' />
             </svg>
           </button>
-
-          
-          <input onChange={(e) => setUser(e.target.value)} type="text" className=' p-3 h-16 w-1/3  bg-[#20293a] rounded-r-lg outline-none text-white' placeholder='Username' />
+          <input
+            onChange={(e) => setUser(e.target.value)}
+            type='text'
+            className='p-3 h-16 sm:w-1/3 bg-[#20293a] rounded-r-lg outline-none text-white'
+            placeholder='Username'
+          />
         </div>
       </div>
-      <DetailsPerfil data={data}/>
-      <Card />
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <DetailsPerfil data={data} />
+        <Card repos={repos} fetchRepos={fetchRepos} />
+      </div>
     </div>
-
-
-
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/components/DetailsPerfil.jsx
+++ src/components/DetailsPerfil.jsx
@@ -1,45 +1,37 @@
-import React from 'react'
-import gitHub from '../assets/github.jpg'
+import React from 'react';
 
-export const DetailsPerfil = ({data}) => {
-
-
-    
+export const DetailsPerfil = ({ data }) => {
   return (
-<>
-    {data &&(
-        <div className='flex justify-evenly w-full h-40  text-[#CDD5E0] '>
-        <div className='w- flex flex-col'>
-          <figure className='w-32 h-32 absolute top-[33%]'>
-            <img src={data.avatar_url} alt="GitHub" className='rounded-lg border-solid border-[10px] border-[#20293A] ' />
-          </figure>
-          <h1 className='font-bold text-5xl  mt-20 mb-2'>GitHub</h1>
-          <p className='text-xl'>How people build software</p>
-        </div>
+    <div className='flex flex-col md:flex-row justify-evenly items-start w-full text-[#CDD5E0]'>
+      {data && (
+        <>
+          <div className='flex flex-col items-center'>
+            <figure className='w-32 h-32 md:absolute md:top-[33%]'>
+              <img src={data.avatar_url} alt="Avatar" className='rounded-lg border-solid border-[10px] border-[#20293A]' />
+            </figure>
+            <h1 className='font-bold text-5xl mt-16 mb-2'>{data.login}</h1>
+            <p className='text-xl'>{data.bio}</p>
+          </div>
 
-        <div className='mt-5 bg-[#111729] px-4 py-2 rounded-lg h-14 w-60 flez items-center text-2xl'>
-          <span className='mr-10 font-normal text-[#4a5366] '>Followers</span>
-          <span className='ml-5'>{data.followers}</span>
-        </div>
+          <div className='mt-5 bg-[#111729] px-4 py-2 rounded-lg md:w-60 md:ml-4 flex items-center text-2xl'>
+            <span className='mr-2 font-normal text-[#4a5366]'>Followers:</span>
+            <span>{data.followers}</span>
+          </div>
 
-        <div className='mt-5 bg-[#111729] px-4 py-2 rounded-lg h-14 w-60 flez items-center text-2xl'>
-          <span className='mr-10 font-normal text-[#4a5366] '>Following</span>
-          <span className='ml-5'>{data.following}</span>
-        </div>
+          <div className=' bg-[#111729] px-4 py-2 rounded-lg md:w-60 md:ml-4 mt-2 md:mt-5 flex items-center text-2xl'>
+            <span className='mr-2 font-normal text-[#4a5366]'>Following:</span>
+            <span>{data.following}</span>
+          </div>
 
-        <div className='mt-5 bg-[#111729] px-4 py-2 rounded-lg h-14 max-w flez items-center text-2xl'>
-          <span className='mr-5 font-normal text-[#4a5366] '>Location</span>
-          <span className='ml-5'>{data.location}</span>
-        </div>
-      </div>
+          <div className=' bg-[#111729] px-4 py-2  rounded-lg md:w-60 md:ml-4 mt-2 md:mt-5 flex items-center text-2xl'>
+            <span className='mr-2 font-normal text-[#4a5366]'>Location:</span>
+            <span>{data.location}</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-    )
-
-    }
-  
-
-</>
-  )
-}
 
 
